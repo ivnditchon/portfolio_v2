6b02465010ab6aadf1cc5361fb1bb26c399d6a5c
Ivan Ditchon <ivnditchon@gmail.com>
2022-02-26 01:07:45 +0800

    Creating folder for social media

---
+++ src/images/social_media/social_media.js
@@ -0,0 +1,21 @@
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import facebook from './facebook.svg'
+
+const socialMedia = {
+    icons: {
+        github: github,
+        linkedin: linkedin,
+        twitter: twitter,
+        facebook: facebook
+    },
+    links: {
+        github: 'https://github.com/ivnditchon',
+        linkedin: 'https://www.linkedin.com/in/ivan-ditchon-824b51219/',
+        twitter: 'https://twitter.com/ivnditchon',
+        facebook: 'https://www.facebook.com/ivnditchon'
+    }
+};
+
+export default socialMedia;
