62d54270b940c6179bd0392917b896aab8de21fc
Ivan Ditchon <ivnditchon@gmail.com>
2022-02-26 01:20:55 +0800

    Updating file name of Project components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./components/Home";
 import About from "./components/About";
-import Projects from "./components/ Projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import sliderData from './components/slider/sliderData';
 
