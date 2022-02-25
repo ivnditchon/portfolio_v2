11ec358583294549c36ad4236c27e2afe406383e
Ivan Ditchon <ivnditchon@gmail.com>
2022-02-26 01:28:39 +0800

    Creating footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (  
+        <div className="flex justify-center relative bottom-0 py-2.5 bg-black opacity-95">
+            <p className="text-xs text-gray-200">ivanditchon-porfolio 2.0 &copy; 2022</p>
+        </div>
+    );
+}
+ 
+export default Footer;
