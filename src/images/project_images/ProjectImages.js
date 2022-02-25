d833517962cfb92dcd943597f9306cd6e56f379a
Ivan Ditchon <ivnditchon@gmail.com>
2022-02-26 01:07:00 +0800

    Creating folder for project images

---
+++ src/images/project_images/ProjectImages.js
@@ -0,0 +1,9 @@
+import todoListImg from './todolist-image.png';
+import ninjaVanImg from './ninjavan-image.png';
+import backburnShopImg from './backburnshop-image.png';
+import torresAndLauban from './t&l-image.png';
+import invitation from './invitation-image.png';
+
+const projectImageList = [todoListImg, ninjaVanImg, backburnShopImg, torresAndLauban, invitation];
+
+export default projectImageList;
