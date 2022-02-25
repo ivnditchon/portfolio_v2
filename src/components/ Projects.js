4940a4664a579fe083820fa86f787c7348e94566
Ivan Ditchon <ivnditchon@gmail.com>
2022-02-26 01:18:00 +0800

    Deleted

--- src/components/ Projects.js
+++
@@ -1,9 +0,0 @@
-const Projects = () => {
-    return (
-        <div role="region" className="w-full h-max mt-20 relative bg-white">
-        
-        </div>  
-    );
-}
- 
-export default Projects;
--- src/social_media_icons/icons.js
+++
@@ -1,8 +0,0 @@
-const socialMediaIcons = {
-    facebookIcon: require('./facebook.svg').default,
-    twitterIcon: require('./twitter.svg').default,
-    githubIcon: require('./github.svg').default,
-    linkedInIcon: require('./linkedin.svg').default
-}
-
-export default socialMediaIcons;
