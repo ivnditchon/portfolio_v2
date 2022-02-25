28ae578f1b7ede4ee6b30906b5f06db7ee0719a6
Ivan Ditchon <ivnditchon@gmail.com>
2022-02-26 01:10:50 +0800

    Importing slideData

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/ Projects";
 import Contact from "./components/Contact";
+import sliderData from './components/slider/sliderData';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Home />
       <div className="w-full h-max px-5 py-10">
         <About />
-        <Projects />
+        <Projects slides={sliderData}/>
         <Contact />
       </div>
     </div>
